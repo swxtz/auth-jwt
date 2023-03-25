53e560c56a9a1eb98fe9d7e422dc822a02a016d6
Gustavo Mendonça <alf4r6@gmail.com>
2023-03-24 22:06:36 -0300

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bg: {
+          "500": "#F0F2F5"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
